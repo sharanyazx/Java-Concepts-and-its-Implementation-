277f2204f5f8a290cf206c7900d977b00dd91015
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-02-02 09:03:06 -0800

    Create Deletion at First and Last.java

---
+++ DSA/Singly LinkedList /Deletion at First and Last.java
@@ -0,0 +1,104 @@
+class LinkedList {
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }   // constructor close
+    }  // class node close
+
+    Node head,tail;  // Define head at the LinkedList level
+
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        // if list is empty
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        // else part --> not empty
+        newNode.next = head;
+        head = newNode;
+    }
+    // function to add at last
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        // if list is empty
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        //else part --> not empty
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public void deleteFirst(){
+        if(head == null){
+            System.out.print(" List is Empty ");
+            return ;
+        }
+        head=head.next;
+    }
+    public  void deleteLast(){
+        if(head==null){
+            System.out.print(" List is empty");
+        }
+        else if(head.next==null){
+            head=null;
+            return;
+        }
+        else{
+            Node temp=head;  // temp points first node
+            Node tail=head.next;  // tail points Second node
+            while(tail.next!=null){  // loop until tail last node
+                temp=temp.next;  // each iteration head update to its 1 step next
+                tail=tail.next;  // each iteration second node of head update to its 1 step next
+            }
+            temp.next=null;  // last node is tail before is temp so next to temp link is removed by assigning to null
+        }
+    }
+
+    // function to Display
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " --> ");
+            current = current.next;
+        }
+        System.out.println("NULL");  // end of LinkedList NULL printing
+    }
+    // main function
+    public static void main(String[] args) {
+        LinkedList link = new LinkedList();
+        // function calling
+        System.out.println("First Insertion ");
+        link.addFirst("Sharanya's");
+        link.printList();
+
+        System.out.println("Insertion at  First");
+        link.addFirst("is");
+        link.printList();
+
+        System.out.println("Insertion at First ");
+        link.addFirst("who");
+        link.printList();
+
+        System.out.println("Last Insertion ");
+        link.addLast("Best Friend ");
+        link.printList();  // Output -- "  who--> is--> Sharanya's--> Best Friend "
+
+        System.out.println("First Deletion  ");
+        link.deleteFirst();
+        link.printList();
+
+        System.out.println("Last Deletion  ");
+        link.deleteLast();
+        link.printList();
+    }
+}
