d6c6bce9f5c2a8abda05c17ad477284e8e7e531a
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-02-07 11:22:22 +0530

    Update Bubble Sort.java

--- Sorting and Searching/Sorting/Bubble Sort.java
+++ Sorting and Searching/Sorting/Bubble Sort.java
@@ -23,3 +23,30 @@ public class Main
 
 }
 }
+
+
+/*
+public class Main
+{
+	public static void main(String[] args) {
+	    int a[]={3,4,1,2,3};
+	    BubbleSort(a);
+
+	}
+	public static void BubbleSort(int []a){
+	    for(int i=0;i<a.length;i++){
+	        for(int j=0;j<a.length-1-i;j++){
+	            if(a[j]>a[j+1]){
+	            int temp=a[j];
+	            a[j]=a[j+1];
+	            a[j+1]=temp;
+	        }
+	    }
+	}
+	
+	for(int aa:a){
+	    System.out.print(aa+" " );
+	}
+}
+}
+*/
