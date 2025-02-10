b9c13ffa50bb065812799cb34c15e042353421f5
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-02-10 13:21:06 +0530

    Create Voting eligiblity .java

---
+++ Exception Handling /Voting eligiblity .java
@@ -0,0 +1,18 @@
+package demo;
+
+public class SS {
+
+    public static void main(String[] args) {
+        int age = 18;
+
+        try {
+            if (age >= 18) {
+                System.out.print("Eligible for voting ");
+            } else {
+                System.out.print("Not eligible for voting ");
+            }
+        } catch (Exception e) {
+            System.out.print("An error occurred:");
+        }
+    }
+}
